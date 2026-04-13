762c0d58cf26292f816ece372e136b2717c5f0c9
dt89 <tamalsarkartwoooo@gmail.com>
2026-04-13 14:32:52 +0000

    add: register service

---
+++ src/common/utils/password.utils.js
@@ -0,0 +1,16 @@
+import bcrypt from "bcryptjs";
+import crypto from 'crypto'
+
+export async function hashPassword(clearTextPassword) {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(clearTextPassword, salt);
+    return hash;
+}
+
+export async function comparePassword(clearTextPassword, hash) {
+    return await bcrypt.compare(clearTextPassword, hash);
+}
+
+export async function hashToken(token) {
+    crypto.createHash("sha256").update(String(token)).digest("hex");
+}
--- src/modules/auth/services.js
+++ src/modules/auth/services.js
@@ -1,5 +1,35 @@
-import db from '../../common/db/db.js'
+import db from "../../common/db/db.js";
+import ApiError from "../../common/utils/api-error.js";
+import { generateResetToken } from "../../common/utils/jwt.utils.js";
+import { hashPassword } from "../../common/utils/password.utils.js";
 
-const register = async() => {
+const register = async ({ name, email, password }) => {
+    // 1. check existing user
+    const existing = await db.query("SELECT * FROM users WHERE email = $1", [email]);
 
-}
+    if (existing.rowCount > 0) {
+        throw ApiError.conflict("Email already exists");
+    }
+
+    // 2. generate verification token
+    const { rawToken, hashedToken } = generateResetToken();
+
+    // 3. hash password
+    const hashedPassword = await hashPassword(password);
+
+    // 4. insert user + RETURNING
+    const result = await db.query(
+        `INSERT INTO users(name, email, password, verificationToken)
+         VALUES($1, $2, $3, $4)
+         RETURNING id, name, email, createdAt`,
+        [name, email, hashedPassword, hashedToken]
+    );
+
+    const user = result.rows[0]
+
+    // 5. return safe data + raw token (email verification can be implimented further)
+    return {
+        user,
+        verificationToken: rawToken,
+    };
+};
